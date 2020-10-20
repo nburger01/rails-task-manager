7722a55c6b6539b6665b98f0008db7f18b2e2052
Naelle Burger <naelle.burger@gmx.at>
2020-10-20 17:21:20 +0200

    task manager works

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show'
+  # get 'tasks/new', to: 'tasks#new', as: :task
+  # post 'tasks', to: 'tasks#create'
 end
